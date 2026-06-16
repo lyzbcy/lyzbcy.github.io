ca98633254dc804c1a581b9f6a366bbe3daec4bf
lyzbcy <lyzbcy@qq.com>
2026-06-16 13:42:37 +0800

    deploy: 恢复Chirpy博客首页 + 顶部世界入口banner

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1781452502',resources: [
+    cacheName: 'chirpy-1781588342',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
