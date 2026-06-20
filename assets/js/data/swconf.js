4e8f1832b796e8842f297dd7fe205d8d6abb876a
lyzbcy <lyzbcy@qq.com>
2026-06-20 17:44:52 +0800

    deploy: AR邀请函怎么玩引导+下载图

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1781948387',resources: [
+    cacheName: 'chirpy-1781948689',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
