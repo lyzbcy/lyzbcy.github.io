c8be33e790ac708d524644e9a8a330e4b1372b86
lyzbcy <lyzbcy@qq.com>
2026-06-23 21:03:03 +0800

    chore: 清理误提交的decrypted_dump.html

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1782219676',resources: [
+    cacheName: 'chirpy-1782219781',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
