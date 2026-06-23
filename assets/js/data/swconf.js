60514a129cdbe4b76768d307d4393134d5cbe9c0
lyzbcy <lyzbcy@qq.com>
2026-06-23 17:00:59 +0800

    deploy: 移除误发布的test目录

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1782204984',resources: [
+    cacheName: 'chirpy-1782205234',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
