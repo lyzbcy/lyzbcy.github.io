8a09d2219612408824a6175d8d9d0c5f831df4cf
lyzbcy <lyzbcy@qq.com>
2026-06-23 20:46:17 +0800

    fix: 加密文章正文排版(content类) + 无锡地图节点去重

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1782218320',resources: [
+    cacheName: 'chirpy-1782218743',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
