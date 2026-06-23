4c351c3b1dc9bf553b6025fc00dedc45e7526dc0
lyzbcy <lyzbcy@qq.com>
2026-06-23 17:15:29 +0800

    deploy: 无锡两日游文章上线（日期改为6.23）

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1782205234',resources: [
+    cacheName: 'chirpy-1782206094',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
