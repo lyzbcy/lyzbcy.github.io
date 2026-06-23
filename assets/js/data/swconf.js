9663867c8accd028ef82f1eb29edd3245dc2bc8f
lyzbcy <lyzbcy@qq.com>
2026-06-23 20:39:05 +0800

    deploy: 无锡两日游行程更新（滨湖万象汇亚朵+鼋头渚蠡园线）

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1782206094',resources: [
+    cacheName: 'chirpy-1782218320',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
