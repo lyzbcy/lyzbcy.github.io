cf70a1310555e927e8e9b86f91775ac6dd66565f
lyzbcy <lyzbcy@qq.com>
2026-08-23 00:42:57 +0800

    deploy: 移除500天文章中的日期明文（密保防泄露）

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1787414078',resources: [
+    cacheName: 'chirpy-1787416922',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
